7dfe88fb96cece404ceee0909eb05d39c8d4ae2c
Anubhav Kandiyal <anubhav.kandiyal@agrostar.in>
2025-04-26 21:06:45 +0530

    installed ShadCN

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,15 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import ChartDisplay from "./components/ChartDisplay";
 import ExpenseForm from "./components/ExpenseForm";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 function App() {
   const [expenses, setExpenses] = useState([]);
@@ -25,6 +34,26 @@ function App() {
       <h1>BudgetFlow</h1>
       <ExpenseForm onAdd={handleAddExpense} />
       <ChartDisplay data={expenses} />
+
+      <Table>
+        <TableCaption>A list of your recent invoices.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Invoice</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Method</TableHead>
+            <TableHead className="text-right">Amount</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-medium">INV001</TableCell>
+            <TableCell>Paid</TableCell>
+            <TableCell>Credit Card</TableCell>
+            <TableCell className="text-right">$250.00</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
     </div>
   );
 }
